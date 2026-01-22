57a4b39b2deacdf55c29266fb9504afd80e77e66
HXD3D0323 <149938316+HXD3D0323@users.noreply.github.com>
2026-01-22 12:30:45 +0800

    Update figure1.R

--- plot_script/figure1.R
+++ plot_script/figure1.R
@@ -1,4 +1,4 @@
-## figure 1B
+##--- figure 1B
 library(Seurat)
 library(scRNAtoolVis)
 library(tidydr)
@@ -37,8 +37,8 @@ DimPlot(scRNA_harmony_used,
         panel.grid.minor = element_blank())+theme(legend.position = "none")
 dev.off()
 
-## figure 1C
-
+##--- figure 1C
+# (python)
 from os.path import join
 import scanpy as sc
 import pandas as pd
@@ -83,8 +83,89 @@ gs = sc.pl.matrixplot(ann,marker_genes, groupby='bulk_labels')
 gs = sc.pl.matrixplot(ann,marker_genes, groupby='bulk_labels')
 gs = sc.pl.matrixplot(ann,marker_genes, groupby='bulk_labels',standard_scale='var',use_raw=False, save="Total.pdf")
 
+##--- figure 1D
+# proportion in tumor sample
+library(harmony)
+library(Seurat)
+library(ggplot2)
+library(patchwork)
+library(dplyr)
+library(hdf5r)
+library(cowplot)
+library(data.table)
+library(plyr)
 
+data<-read.table("F:/23.肾包膜项目/02.重新聚类/Cell_Number.csv",sep=",",header=T,check.names=F)
 
+data<-subset(data,Region %in% c("Cancer"))
+
+data$Cell <- factor(data$Cell, levels = c("Mast","B/Plasma","PT","Fibroblast","Endothelial","NK","Myeloid","T","Epithelial"))
+forestCol=c("T"="#E95D53","NK"="#F9F871","B/Plasma"="#ECB37B",
+"Myeloid"="#D3ACAE","Mast"="#D1D198","Endothelial"="#3EAC71","Fibroblast"="#4FFBDF","PT"="#C5E7A4",
+"Epithelial"="#5867AF")
+
+data$Proportion<-0
+
+for(i in 1:nrow(data)){
+	data[i,"Proportion"]=data[i,"Number"]/sum(data$Number)
+}
+data$Proportion<-data$Proportion*100
+ggplot(data=data,mapping=aes(x=Cell,y=Proportion,fill=Cell))+
+theme_bw()+#theme(axis.text.x=element_text(angle=45,hjust = 1,vjust=1))+
+labs(x="",y="Cell proportion (%)")+scale_y_continuous(limits = c(0, 40),breaks = seq(0, 40,10))+
+scale_fill_manual(values=forestCol)+coord_flip()+
+#coord_polar()+这是变成旋转的图
+ geom_bar(stat="identity")+theme(legend.position="none")+theme(panel.grid.major = element_blank())+theme_classic()+
+ theme( axis.text.y = element_text(size = 18,colour = 'black'),#这是更改了y轴的标识的大小
+        axis.text.x = element_text(size = 18,colour = 'black'),
+		axis.title.x = element_text(size = 15,colour = 'black'),
+        axis.title.y = element_text(size = 15,colour = 'black'),
+		axis.line = element_line(size = 1.0)
+		)+theme (legend.position = 'none')
+		
+ggsave("02.Region_Cancer_Proportion.pdf",device = "pdf",width = 5.5,height =4.5)
+
+# proportion in capsule sample
+library(harmony)
+library(Seurat)
+library(ggplot2)
+library(patchwork)
+library(dplyr)
+library(hdf5r)
+library(cowplot)
+library(data.table)
+library(plyr)
+
+data<-read.table("F:/23.肾包膜项目/02.重新聚类/Cell_Number.csv",sep=",",header=T,check.names=F)
+
+data<-subset(data,Region %in% c("Capsule"))
+
+data$Cell <- factor(data$Cell, levels = c("PT","B/Plasma","Mast","Endothelial","Fibroblast","NK","Myeloid","Epithelial","T"))
+forestCol=c("T"="#E95D53","NK"="#F9F871","B/Plasma"="#ECB37B",
+"Myeloid"="#D3ACAE","Mast"="#D1D198","Endothelial"="#3EAC71","Fibroblast"="#4FFBDF","PT"="#C5E7A4",
+"Epithelial"="#5867AF")
+
+data$Proportion<-0
+
+for(i in 1:nrow(data)){
+	data[i,"Proportion"]=data[i,"Number"]/sum(data$Number)
+}
+data$Proportion<-data$Proportion*100
+ggplot(data=data,mapping=aes(x=Cell,y=Proportion,fill=Cell))+
+theme_bw()+#theme(axis.text.x=element_text(angle=45,hjust = 1,vjust=1))+
+labs(x="",y="Cell proportion (%)")+scale_y_continuous(limits = c(0, 50),breaks = seq(0, 50,10))+
+scale_fill_manual(values=forestCol)+coord_flip()+
+#coord_polar()+这是变成旋转的图
+ geom_bar(stat="identity")+theme(legend.position="none")+theme(panel.grid.major = element_blank())+theme_classic()+
+ theme( axis.text.y = element_text(size = 18,colour = 'black'),#这是更改了y轴的标识的大小
+        axis.text.x = element_text(size = 18,colour = 'black'),
+		axis.title.x = element_text(size = 15,colour = 'black'),
+        axis.title.y = element_text(size = 15,colour = 'black'),
+		axis.line = element_line(size = 1.0))+theme (legend.position = 'none')
+		
+ggsave("02.Region_Capsule_Proportion.pdf",device = "pdf",width = 5.5,height =4.5)
+
+##--- figure 1E
 
 
 
